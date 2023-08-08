33b0d6428a3fae3ca9285fd2ba96ebbc32076780
Ylfa Liang <111717563+yingfangliang@users.noreply.github.com>
2023-08-07 22:16:09 -0400

    added materialization (models)

---
+++ dbtproj/models/dim/dim_listings_with_hosts.sql
@@ -0,0 +1,20 @@
+WITH 
+l AS (
+    SELECT * FROM {{ ref('dim_listings_cleaned') }}
+),
+h AS (
+    SELECT * FROM {{ ref('dim_hosts_cleaned') }}
+)
+SELECT
+    l.listing_id,
+    l.listing_name,
+    l.room_type,
+    l.minimum_nights,
+    l.price,
+    l.host_id,
+    h.host_name,
+    h.is_superhost as host_is_superhost,
+    l.created_at,
+    GREATEST(l.updated_at, h.updated_at) as updated_at
+FROM l
+LEFT JOIN h ON (h.host_id = l.host_id)
---
+++ dbtproj/models/fct/fct_reviews.sql
@@ -0,0 +1,17 @@
+{{ 
+    config(
+        materialized = 'incremental',
+        on_schema_change = 'fail'
+    )
+}}
+
+WITH src_reviews AS (
+    SELECT * FROM {{ ref('src_reviews') }}
+)
+SELECT * 
+FROM src_reviews
+WHERE review_text IS NOT NULL
+
+{% if is_incremental() %}
+    AND review_date > (SELECT MAX(review_date) FROM {{this}})
+{% endif %}
