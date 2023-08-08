3cc87f8689e1ee5c793993d8b5ba41a188aa1117
Ylfa Liang <111717563+yingfangliang@users.noreply.github.com>
2023-08-08 16:12:51 -0400

    added analyses and dashboards

--- dbtproj/tests/consistent_created_at.sql
+++ dbtproj/tests/consistent_created_at.sql
@@ -4,7 +4,7 @@ FROM {{ ref('fct_reviews') }} AS f
     ON f.listing_id = l.listing_id
 WHERE f.review_date < l.created_at
 
--- SELECT * FROM {{ ref('dim_listings_cleansed') }} l
+-- SELECT * FROM {{ ref('dim_listings_cleaned') }} l
 -- INNER JOIN {{ ref('fct_reviews') }} r
 -- USING (listing_id)
 -- WHERE l.created_at >= r.review_date
